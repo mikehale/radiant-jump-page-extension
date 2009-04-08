832626e56af84d217e97017749bbcd53a71a7704
Michael Hale <mikehale@gmail.com>
2009-04-08 17:35:07 -0400

    handle non existant config part

--- app/models/jump_page.rb
+++ app/models/jump_page.rb
@@ -2,7 +2,8 @@ class JumpPage < Page
   attr_accessor :last_url, :last_title
 
   def config
+    debugger
     config = self.render_part(:config)
-    config.empty? ? {:exclude => []} : YAML::load(config).symbolize_keys
+    (config.nil? || config.empty?) ? {:exclude => []} : YAML::load(config).symbolize_keys
   end
 end
