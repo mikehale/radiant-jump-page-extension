49dab1bfc128be7032f4bb7334e4ae2553804584
Michael Hale <mikehale@gmail.com>
2009-05-15 17:03:38 -0400

    remove debugger

--- app/models/jump_page.rb
+++ app/models/jump_page.rb
@@ -2,7 +2,6 @@ class JumpPage < Page
   attr_accessor :last_url, :last_title
 
   def config
-    debugger
     config = self.render_part(:config)
     (config.nil? || config.empty?) ? {:exclude => []} : YAML::load(config).symbolize_keys
   end
